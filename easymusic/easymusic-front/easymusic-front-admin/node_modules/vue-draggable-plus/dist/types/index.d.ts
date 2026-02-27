16530ec9e83102c6cc0e4241a3f26fd24bed19e1
qingnianzzz <qingnianzzzz@gmail.com>
2026-02-27 15:06:43 +0800

    Delete easymusic/easymusic-front/easymusic-front-admin/node_modules directory

--- easymusic/easymusic-front/easymusic-front-admin/node_modules/vue-draggable-plus/dist/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Ref, ShallowRef, WritableComputedRef } from 'vue';
-export { type Options, type SortableEvent, type MoveEvent } from 'sortablejs';
-/**
- * copied from vueuse: https://github.com/vueuse/vueuse/blob/main/packages/shared/tryOnUnmounted/index.ts
- * Maybe it's a ref, or a plain value.
- */
-export type MaybeRef<T = any> = T | Ref<T> | ShallowRef<T> | WritableComputedRef<T>;
-export type RefOrElement<T = HTMLElement> = T | Ref<T | undefined | null> | string;
-export type Fn = (...args: any[]) => any;
